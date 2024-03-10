2610fc9f7f78ef540af1f2ecb599092e697beb0a
Wonjun Lee (Jake Lee) <31210124+workoutplz@users.noreply.github.com>
2024-03-10 17:19:34 +0900

    feat: comments route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import groupRoutes from './src/routes/groupRoutes.js';
 import postRoutes from './src/routes/postRoutes.js';
+import commentRoutes from './src/routes/commentRoutes.js';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/api', groupRoutes);
 app.use('/api', postRoutes);
+app.use('/api', commentRoutes);
 
 app.listen(3001);
---
+++ src/routes/commentRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import CommentController from '../controllers/commentController.js';
+
+const commentRoutes = express.Router();
+
+commentRoutes
+    .route('/posts/:postId/comments')
+    .get(CommentController.getAllComments);
+
+commentRoutes
+    .route('/posts/:postId/comments')
+    .post(CommentController.createComment);
+
+commentRoutes
+    .route('/comments/:commentId')
+    .put(CommentController.updateComment);
+
+commentRoutes
+    .route('/comments/:commentId')
+    .delete(CommentController.deleteComment);
+
+export default commentRoutes;
