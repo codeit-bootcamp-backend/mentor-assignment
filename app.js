512e33c43c4ff66bd432dca2a2a39bc95577636c
Wonjun Lee (Jake Lee) <31210124+workoutplz@users.noreply.github.com>
2024-03-16 16:04:35 +0900

    fix: routes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import groupRoutes from './src/routes/groupRoutes.js';
 import postRoutes from './src/routes/postRoutes.js';
 // import commentRoutes from './src/routes/commentRoutes.js';
-import baseRoute from './src/routes/baseRoutes.js';
+import baseRoutes from './src/routes/baseRoutes.js';
 
 const app = express();
 const baseRoute = '/api';
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use(baseRoute, groupRoutes);
 app.use(baseRoute, postRoutes);
 // app.use(baseRoute, commentRoutes);
-app.use(baseRoute, baseRoute);
+app.use(baseRoute, baseRoutes);
 
 app.listen(3001);
