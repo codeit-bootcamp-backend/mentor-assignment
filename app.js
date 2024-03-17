1d75a695ee66c4d8fdafb65f76eb099b1919133a
Wonjun Lee (Jake Lee) <31210124+workoutplz@users.noreply.github.com>
2024-03-17 23:34:30 +0900

    fix: cors

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import groupRoutes from './src/routes/groupRoutes.js';
 import postRoutes from './src/routes/postRoutes.js';
 // import commentRoutes from './src/routes/commentRoutes.js';
 import baseRoutes from './src/routes/baseRoutes.js';
+import cors from 'cors';
 
 const app = express();
 const baseRoute = '/api';
 
 app.use(express.json());
+app.use(cors());
 
 app.use(baseRoute, groupRoutes);
 app.use(baseRoute, postRoutes);
