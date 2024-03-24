a3bac9c13136a132e2fdcc4986d01890b03915a9
Wonjun Lee (Jake Lee) <31210124+workoutplz@users.noreply.github.com>
2024-03-24 17:27:44 +0900

    fix: upload image

--- src/routes/baseRoutes.js
+++ src/routes/baseRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import multer from 'multer';
 import BaseController from '../controllers/baseController.js';
+import uuid4 from 'uuid4';
 
 const postRoutes = express.Router();
 const upload = multer({
     storage: multer.diskStorage({
         filename(req, file, done) {
-            done(null, file.originalname);
+            const randomId = uuid4();
+            const ext = file.originalname.split('.').pop();
+            done(null, `${randomId}.${ext}`);
         },
 
         destination(req, file, done) {
